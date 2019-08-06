1d2a4fab98f475dc5f6f0ed97fd2036bd51a4380
Eleasah Halsmer <eleasahjh@gmail.com>
2019-08-06 14:21:31 -0600

    Updating userData and followersUrl from first axios call in componentDidMount

--- react-github-user-card/src/App.js
+++ react-github-user-card/src/App.js
@@ -6,20 +6,31 @@ class App extends React.Component {
   constructor(){
     super();
     this.state={
-      user: {},
-      followers: []
+      user: 'ehalsmer',
+      userData: {},
+      followersUrl: '',
+      followersArray: []
     }
   }
 
   componentDidMount(){
-
+    axios.get(`https://api.github.com/users/${this.state.user}`)
+    .then((response)=>{
+      console.log('axios response', response.data)
+      this.setState({
+        userData: response.data,
+        followersUrl: response.data.followers_url
+      })
+    })
   }
 
   render(){
-
+    console.log('state in render', this.state)
     return (
       <div className="App">
-        <h1>User Card</h1>
+        {/* // Render userCard component, passing userData object as prop */}
+        <h1>{this.state.userData.login}</h1>
+        {/* Use .map on followers array and render a card component for each follower object */}
         <h2>Follower Cards</h2>
       </div>
     );
